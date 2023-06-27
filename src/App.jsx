16972d54d1340583a4043a70428c35f546fa03e3
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-27 18:21:16 -0400

    Project - App - fetching coutries and maping through them

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+// api
+import { fetchRequest } from "./api"
+
+// components
+import Container from "./components/Container.jsx"
+import CountrieCard from "./components/CountrieCard/CountrieCard.jsx"
 
 function App() {
+  // app state
+  const [countries, setCountries] = useState([])
+
+  // component mounted
+  useEffect(() => {
+    let mounted = true
+    async function fetchCountries() {
+      try {
+        const response = await fetchRequest('all')
+        if (mounted) {
+          console.log(response[0])
+          setCountries(response)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchCountries()
+    return () => mounted = false
+  }, [])
+
   return (
-    <div>
-      We are ready to go......
-    </div>
+    <Container styles="">
+      <Container styles="grid gap-14 p-14 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        {countries.map((countrie, index) => <CountrieCard key={index} countrie={countrie} />)}
+      </Container>
+    </Container>
   )
 }
 

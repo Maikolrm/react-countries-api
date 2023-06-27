a84b67642fe9b75f0ac437efb16df8c7c786b7ac
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-27 18:19:15 -0400

    Project - api - creating file and fetchRequest method

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+// base url
+axios.defaults.baseURL = `https://restcountries.com/v3.1/`
+
+export const fetchRequest = async function(route) {
+  try {
+    const response = await axios.get(route)
+    return response.data
+  } catch(e) {
+    return e
+  }
+}

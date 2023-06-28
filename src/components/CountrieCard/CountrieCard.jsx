a2eac2c3d36bd1ea4d5f6245490620f24343560b
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-28 12:37:26 -0400

    Components - CountrieCard - styling component at dark mode

--- src/components/CountrieCard/CountrieCard.jsx
+++ src/components/CountrieCard/CountrieCard.jsx
@@ -4,20 +4,20 @@ import CountrieFlag from "./CountrieFlag.jsx"
 
 export default function CountrieCard({ countrie }) {
   return(
-    <Container styles="bg-white shadow-xl rounded-lg overflow-hidden">
+    <Container styles="bg-white shadow-xl rounded-lg overflow-hidden dark:bg-element-dark">
       <CountrieFlag countrie={countrie} />
       <Container styles="p-4">
-        <h2 className="text-xl font-bold capitalize text-gray-900 leading-10">
+        <h2 className="text-xl font-bold capitalize text-gray-900 leading-10 dark:text-gray-200">
           {countrie.name.common}
         </h2>
-        <Container styles="">
-          <p className="text-md capitalize text-gray-700 leading-10">
+        <Container styles="text-dark-blue dark:text-gray-200">
+          <p className="text-md capitalize leading-10">
             <span className="font-semibold">population:</span> {countrie.population}
           </p>
-          <p className="text-md capitalize text-gray-700 leading-10">
+          <p className="text-md capitalize leading-10">
             <span className="font-semibold">region:</span> {countrie.region}
           </p>
-          <p className="text-md capitalize text-gray-700 leading-10">
+          <p className="text-md capitalize leading-10">
             <span className="font-semibold">capital:</span> {countrie.capital}
           </p>
         </Container>

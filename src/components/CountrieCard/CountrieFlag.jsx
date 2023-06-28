43ee4611d90a46ee535aba5956408b7d8cdd0250
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-28 12:36:54 -0400

    Components - CountrieFlag - removing unnecesary bg

--- src/components/CountrieCard/CountrieFlag.jsx
+++ src/components/CountrieCard/CountrieFlag.jsx
@@ -3,7 +3,7 @@ import Container from "../Container.jsx"
 
 export default function CoutrieFlag({ countrie }) {
   return(
-    <Container styles="h-64 bg-gray-100 xl:h-[15vw]">
+    <Container styles="h-64 xl:h-[15vw]">
       <img src={countrie.flags.png} alt={countrie.flags.alt} className="w-full h-full object-center" />
     </Container>
   )

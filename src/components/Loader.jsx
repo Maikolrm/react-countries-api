67e809a53eb320d63d2b7e277cff2b0882326e9c
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-28 10:26:57 -0400

    Components - Loader - creating component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+// components
+import Container from "./Container.jsx"
+
+export default function Loader(props) {
+  return(
+    <Container styles="absolute top-0 left-0 z-10 flex w-full h-full bg-white/95">
+      <Container styles="m-auto">
+        <Container styles="w-8 h-8 m-auto rounded-full border-2 border-transparent border-y-sky-500 animate-spin" />
+        <h3 className="mt-2 font-semibold text-xs text-gray-500 uppercase leading-10 tracking-widest">
+          Loading
+        </h3>
+      </Container>
+    </Container>
+  )
+}

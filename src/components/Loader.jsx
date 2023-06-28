42b037532d73889e73fd9e8aea09edfc326a540a
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-28 12:36:19 -0400

    Components - Loader - styling component at dark mode

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,10 +3,10 @@ import Container from "./Container.jsx"
 
 export default function Loader(props) {
   return(
-    <Container styles="fixed top-0 left-0 z-10 flex w-full h-full bg-white/95">
+    <Container styles="fixed top-0 left-0 z-10 flex w-full h-full bg-white/95 dark:bg-dark-bg/95">
       <Container styles="m-auto">
         <Container styles="w-8 h-8 m-auto rounded-full border-2 border-transparent border-y-sky-500 animate-spin" />
-        <h3 className="mt-2 font-semibold text-xs text-gray-500 uppercase leading-10 tracking-widest">
+        <h3 className="mt-2 font-semibold text-xs text-gray-500 uppercase leading-10 tracking-widest dark:text-gray-200">
           Loading
         </h3>
       </Container>

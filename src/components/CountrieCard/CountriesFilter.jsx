ee43c6290f92a827039ca7cd57e505865fd782b1
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-28 11:28:32 -0400

    Components - CountriesFilter - adding visual hint to selected region

--- src/components/CountrieCard/CountriesFilter.jsx
+++ src/components/CountrieCard/CountriesFilter.jsx
@@ -75,7 +75,7 @@ export default function CountriesFilter(props) {
               <button 
                 key={region}
                 type="button"
-                className="block w-full px-5 rounded text-left leading-[3rem] transition duration-100 hover:bg-gray-100"
+                className={`block w-full px-5 ${selectedRegion == region ? 'bg-gray-200' : ''} rounded text-left leading-[3rem] transition duration-100 hover:bg-gray-200`}
                 onClick={() => handleCountrieRegionClick(region)}>
                 {region.slice(0, 1).toUpperCase() + region.slice(1)}
               </button>

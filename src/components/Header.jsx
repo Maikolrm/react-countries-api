83537039a0ec012ac647bc46bb0010215e11b423
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-29 18:42:12 -0400

    Components - Header - wrapping app logo into Link component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { Link } from "react-router-dom"
 
 // context
 import AppState from "../context/AppState.js"
@@ -16,9 +17,11 @@ export default function Header(props) {
 
   return(
     <header className="flex items-center justify-between h-20 px-4 bg-white shadow-md text-dark-blue md:px-14 dark:bg-element-dark dark:text-gray-100">
-      <h1 className="text-lg font-bold lg:text-xl">
-        Where in the world?
-      </h1>
+      <Link to="/">
+        <h1 className="text-lg font-bold lg:text-xl">
+          Where in the world?
+        </h1>
+      </Link>
       <button onClick={() => appDispatch({ type: 'set-dark-mode', value: !darkMode })} type="button" className="flex capitalize text-base">
         <span className="inline-block w-10 -rotate-45 leading-10">
           <i className="fa-solid fa-moon" />

b2854e27c9209af498bddd245879f49df9c7be6d
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-29 18:59:13 -0400

    Pages - countries/CountrieFlag - pullin media rules from props

--- src/pages/countries/components/CountrieFlag.jsx
+++ src/pages/countries/components/CountrieFlag.jsx
@@ -4,7 +4,7 @@ import Container from "../../../components/Container.jsx"
 export default function CoutrieFlag(props) {
   const countrie = props.countrie
   return(
-    <Container styles={`${props.styles} h-64 xl:h-[15vw]`}>
+    <Container styles={`${props.styles} ${props.media}`}>
       <img src={countrie.flags.png} alt={countrie.flags.alt} className="w-full h-full object-center" />
     </Container>
   )

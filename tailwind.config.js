5fb53ff489e86732d64926b1dced30a62598a67c
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-29 20:28:55 -0400

    Project - tailwind - extending theme ( maxWidth and screens )

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ export default {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        'screen-4xl' : '1920px'
+      },
       colors: {
         'light-bg' : 'hsl(0, 0%, 98%)',
         'dark-bg' : 'hsl(207, 26%, 17%)',
         'element-dark' : 'hsl(209, 23%, 22%)',
         'dark-blue' : 'hsl(200, 15%, 8%)'
+      },
+      screens: {
+        '4xl' : '2000px'
       }
     },
   },

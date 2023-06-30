8db6de24e05b9c7602ed28e2678b822b2080adea
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-30 10:43:48 -0400

    Pages - countries/CountrieDetails - finishing responsive desing

--- src/pages/countries/CountrieDetails.jsx
+++ src/pages/countries/CountrieDetails.jsx
@@ -44,47 +44,51 @@ export default function CountrieDetails(props) {
 
   return(
     <Container styles="p-8 px-4 md:px-14">
-      <Container styles="pb-12">
-        <Link to={`/countrie-region/${countrie.region}`} className="flex w-28 leading-10 bg-white rounded-md shadow-lg capitalize text-dark-blue dark:bg-element-dark dark:text-gray-200">
-          <span className="blcok w-10 h-10 text-center leading-10">
-            <i className="fa-solid fa-arrow-left"/>
-          </span>
-          back
-        </Link>
-      </Container>
-      <Container styles="">
-        <CountrieFlag countrie={countrie} styles="rounded-md overflow-hidden" />
-        <Container styles="pt-12">
-          <h2 className="font-bold text-3xl text-dark-blue leading-none dark:text-gray-200">
-            {countrie.name.common}
-          </h2>
-          <Container styles="grid gap-8 mt-8">
-            <Container styles="grid gap-4 text-dark-blue">
-              <Paragraph label='native name' content={Object.values(countrie.name.nativeName)[0].official} />
-              <Paragraph label='population' content={countrie.population} />
-              <Paragraph label='region' content={countrie.region} />
-              <Paragraph label='sub region' content={countrie.subregion} />
-              <Paragraph label='capital' content={countrie.capital} />
-            </Container>
-            <Container styles="grid gap-4">
-              <Paragraph label='top level domain' content={countrie.tld[0]} />
-              <Paragraph label='currencies' content={Object.values(countrie.currencies)[0].name} />
-              <Paragraph label='languages' content={Object.values(countrie.languages).join(', ')} />
-            </Container>
-            {countrie.borders ? (
-              <Container styles="text-dark-blue dark:text-gray-200">
-                <h4 className="font-semibold text-lg capitalize leading-none">
-                  border countries:
-                </h4>
-                <Container styles="grid grid-cols-3 gap-2 pt-8">
-                  {countrie.borders.map(border => (
-                    <Link to={`/countrie-details/${border}`} key={border} className="flex-1 rounded-md border-2 border-gray-200 text-center text-sm leading-10 dark:bg-element-dark dark:border-dark-blue/70">
-                      {border}
-                    </Link>
-                  ))}
+      <Container styles="max-w-2xl m-auto xl:max-w-screen-4xl">
+        <Container styles="pb-12">
+          <Link to={`/countrie-region/${countrie.region}`} className="flex w-28 leading-10 bg-white rounded-md shadow-lg capitalize text-dark-blue dark:bg-element-dark dark:text-gray-200">
+            <span className="blcok w-10 h-10 text-center leading-10">
+              <i className="fa-solid fa-arrow-left"/>
+            </span>
+            back
+          </Link>
+        </Container>
+        <Container styles="xl:grid xl:grid-cols-2 xl:gap-14">
+          <CountrieFlag countrie={countrie} styles="rounded-lg shadow-lg overflow-hidden" media="xl:flex-1 xl:h-[30vw] xl:max-h-[40rem]" />
+          <Container styles="pt-12 xl:flex xl:flex-col xl:flex-1 xl:justify-center xl:pt-0">
+            <h2 className="font-bold text-3xl text-dark-blue leading-none dark:text-gray-200">
+              {countrie.name.common}
+            </h2>
+            <Container styles="grid gap-14 mt-8">
+              <Container styles="text-dark-blue sm:flex">
+                <Container styles="grid gap-6 content-start">
+                  <Paragraph label='native name' content={Object.values(countrie.name.nativeName)[0].official} />
+                  <Paragraph label='population' content={countrie.population} />
+                  <Paragraph label='region' content={countrie.region} />
+                  <Paragraph label='sub region' content={countrie.subregion} />
+                  <Paragraph label='capital' content={countrie.capital} />
+                </Container>
+                <Container styles="grid gap-6 content-start mt-10 sm:mt-0 sm:pl-14">
+                  <Paragraph label='top level domain' content={countrie.tld[0]} />
+                  <Paragraph label='currencies' content={Object.values(countrie.currencies)[0].name} />
+                  <Paragraph label='languages' content={Object.values(countrie.languages).join(', ')} />
                 </Container>
               </Container>
-            ) : ('')}
+              {countrie.borders ? (
+                <Container styles="text-dark-blue dark:text-gray-200">
+                  <h4 className="font-semibold text-lg capitalize leading-none">
+                    border countries:
+                  </h4>
+                  <Container styles="grid grid-cols-3 gap-2 pt-8 sm:grid-cols-4">
+                    {countrie.borders.map(border => (
+                      <Link to={`/countrie-details/${border}`} key={border} className="flex-1 rounded-md border-2 border-gray-200 text-center text-sm leading-10 dark:bg-element-dark dark:border-dark-blue/70">
+                        {border}
+                      </Link>
+                    ))}
+                  </Container>
+                </Container>
+              ) : ('')}
+            </Container>
           </Container>
         </Container>
       </Container>

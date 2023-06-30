7a638e485a1ee1de96df0ae3113bd8d895ff9198
hernandez maikol <hernandez_maikol@protonmail.com>
2023-06-29 20:30:48 -0400

    Components - Header - wrapping header content into Cotntainer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,20 +16,22 @@ export default function Header(props) {
   const appDispatch = useContext(AppDispatch)
 
   return(
-    <header className="flex items-center justify-between h-20 px-4 bg-white shadow-md text-dark-blue md:px-14 dark:bg-element-dark dark:text-gray-100">
-      <Link to="/">
-        <h1 className="text-lg font-bold lg:text-xl">
-          Where in the world?
-        </h1>
-      </Link>
-      <button onClick={() => appDispatch({ type: 'set-dark-mode', value: !darkMode })} type="button" className="flex capitalize text-base">
-        <span className="inline-block w-10 -rotate-45 leading-10">
-          <i className="fa-solid fa-moon" />
-        </span>
-        <span className="inline-block leading-10">
-          dark mode
-        </span>
-      </button>
+    <header className="px-4 bg-white shadow-md text-dark-blue md:px-14 dark:bg-element-dark dark:text-gray-100">
+      <Container styles="flex items-center justify-between h-20 max-w-screen-4xl mx-auto">
+        <Link to="/">
+          <h1 className="text-lg font-bold lg:text-xl">
+            Where in the world?
+          </h1>
+        </Link>
+        <button onClick={() => appDispatch({ type: 'set-dark-mode', value: !darkMode })} type="button" className="flex capitalize text-base">
+          <span className="inline-block w-10 -rotate-45 leading-10">
+            <i className="fa-solid fa-moon" />
+          </span>
+          <span className="inline-block leading-10">
+            dark mode
+          </span>
+        </button>
+      </Container>
     </header>
   )
 }
